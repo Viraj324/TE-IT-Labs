f248a55bc8249df412d4986890df7f8fa5e43979
Viraj <viraj.sonawane22@pccoepune.org>
2023-09-09 13:17:23 +0530

    Changes Before Insem

--- Operating System/RoundRobin.c
+++ Operating System/RoundRobin.c
@@ -1,52 +1,59 @@
 #include <stdio.h>
-#include <conio.h>
 
-void main(){
-    int n,i,qt,count=0,temp,sq=0,bt[10],tat[10],rem_bt[10];
-    float awt =0,atat=0;
-    clrscr();
-    printf("Enter number of Process : ");
-    scanf("%d",&n);
-    printf("Enter the Burst time of process : ");
-    for(i = 0;i<n;i++){
-        scanf("%d",&bt[i]);
+int main() {
+    int n, i, qt, count = 0, temp, sq = 0, bt[10], tat[10], rem_bt[10], wt[10]; // Added 'wt' array for waiting time
+    float awt = 0, atat = 0;
+    
+    printf("Enter number of Processes: ");
+    scanf("%d", &n);
+    
+    printf("Enter the Burst time of processes: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &bt[i]);
         rem_bt[i] = bt[i];
     }
-    printf("Enter The Quantum Time");
-    scanf("%d",&qt);
-    while(1){
-        for(int i = 0,count =0;i<n;i++){
+    
+    printf("Enter The Quantum Time: ");
+    scanf("%d", &qt);
+    
+    while (1) {
+        for (i = 0, count = 0; i < n; i++) {
             temp = qt;
-            if(rem_bt[i] == 0){
+            
+            if (rem_bt[i] == 0) {
                 count++;
                 continue;
             }
-            if(rem_bt[i]>qt){
-                rem_bt[i] = rem_bt[i]-qt;
+            
+            if (rem_bt[i] > qt) {
+                rem_bt[i] = rem_bt[i] - qt;
+            } else {
+                temp = rem_bt[i];
+                rem_bt[i] = 0;
             }
-            else{
-                if(rem_bt[i]>=0){
-                    temp = rem_bt[i];
-                    rem_bt[i] = 0;
-                }
-                sq = sq + temp;
-                tat[i] = sq;
-            }
-            if(n==count){
-                break;
-            }
+            
+            sq = sq + temp;
+            tat[i] = sq;
         }
-        printf("\nprocess\tburst time\tturnaround time\twaiting time\n");
-        for(int i = 0;i<n;i++){
-            wt[i] = tat[i]-bt[i];
-            awt = awt+wt[i];
-            atat = atat+tat[i];
-            printf("\n%d\t%d\t\t%d\t\t%d",i+1,bt[i],tat[i],wt[i]);
+        
+        if (n == count) {
+            break;
         }
-        awt = awt/n;
-        atat = atat/n;
-        printf("Average Awaiting time = %f\n",awt);
-        printf("Average Turnaround time = %f",atat);
-        getch();
     }
+    
+    printf("\nProcess\tBurst Time\tTurnaround Time\tWaiting Time\n");
+    for (i = 0; i < n; i++) {
+        wt[i] = tat[i] - bt[i];
+        awt = awt + wt[i];
+        atat = atat + tat[i];
+        printf("%d\t%d\t\t%d\t\t%d\n", i + 1, bt[i], tat[i], wt[i]);
+    }
+    
+    awt = awt / n;
+    atat = atat / n;
+    printf("Average Waiting Time = %f\n", awt);
+    printf("Average Turnaround Time = %f\n", atat);
+    
+    return 0;
 }
+
